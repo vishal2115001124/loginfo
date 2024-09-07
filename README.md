5e39f9dd42e87dd8339257bfcb90fd2c3eb83a37
vishal@03 <singhalshal42@gmail.com>
2024-09-07 16:37:59 +0530

    f

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Commit Date: 2024-09-11
+# Commit Date: 2024-05-11
 # Random edit on Sat Sep  7 16:32:05 IST 2024
--- commit.sh
+++ commit.sh
@@ -1,30 +1,43 @@
 #!/bin/bash
 
-# # Parameters
-# file_to_edit="README.md"
-# git_remote_url="https://github.com/vishal2115001124/loginfo.git"
-
-#!/bin/bash
+# Parameters
+file_to_edit="README.md"
+git_remote_url="https://github.com/vishal2115001124/loginfo.git"
 
 # Set the start date
 START_DATE="2023-12-10"
 
-# Set the end date to the current date
-END_DATE="2024-07-09"
+# Set the end date to 2024-07-09
+END_DATE="2024-09-07"
+
+# Set the fixed time for all commits
+COMMIT_TIME="16:21:23"
 
 # Set the commit message
 COMMIT_MESSAGE="Random edit on"
 
-# Loop through the date range
+# Create or update the file to commit
+touch "$file_to_edit"
+
+# Loop through the date range and commit every 3 days
 while [ "$START_DATE" != "$END_DATE" ]; do
+  # Create or update README.md file with the commit date
+  echo "# Commit Date: $START_DATE" > "$file_to_edit"
+  
+  # Make a random edit to the file (e.g., add a comment)
+  echo "# Random edit on $(date)" >> "$file_to_edit"
+  
+  # Add changes to git
+  git add "$file_to_edit"
+  
   # Commit changes with the current date
-  git add .
-  git commit -m "$COMMIT_MESSAGE $START_DATE" --date="$START_DATE 16:21:23"
+  GIT_AUTHOR_DATE="$START_DATE $COMMIT_TIME" GIT_COMMITTER_DATE="$START_DATE $COMMIT_TIME" git commit -m "$COMMIT_MESSAGE $START_DATE"
+  
+  # Push the commit to the remote repository
+  git push "$git_remote_url" main
+  
+  echo "Changes committed and pushed for date: $START_DATE"
   
   # Increment the date by 3 days
   START_DATE=$(date -d "$START_DATE + 3 days" +"%Y-%m-%d")
 done
-
-# Commit changes with the current date
-git add .
-git commit -m "$COMMIT_MESSAGE $END_DATE" --date="$END_DATE 16:21:23"
